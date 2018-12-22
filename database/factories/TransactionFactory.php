d11410a33ea3ffb40b6760d1ee2a36ac22e5f42d
Mirko Josimovic <mirkojosimovic1987@gmail.com>
2018-12-22 14:29:35 +0100

    creating factories for database seeder

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+use Faker\Generator as Faker;
+use App\Transaction;
+use App\Seller;
+use App\User;
+
+$factory->define(Transaction::class, function (Faker $faker) {
+
+    $seller = Seller::has('products')->get()->random();
+    $buyer = User::all()->except($seller->id)->random();
+
+    return [
+        'quantity' => $faker->numberBetween(1,3),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id,
+    ];
+});
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
-<?php
+ <?php
 
 use Faker\Generator as Faker;
+use App\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +14,15 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
-    return [
+$factory->define(User::class, function (Faker $faker) {
+    return array(
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
-    ];
+        'verified' => $verified = $faker->randomElement(array(User::VERIFIED_USER, User::UNVERIFIED_USER)),
+        'verification_token' => $verified == User::VERIFIED_USER ? null : User::generateVerificationToken(),
+        'admin' => $verified = $faker->randomElement(array(User::ADMIN_USER, User::REGULAR_USER))
+    );
 });
